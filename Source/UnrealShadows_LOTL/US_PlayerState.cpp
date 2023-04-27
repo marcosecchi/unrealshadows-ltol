67b00d13e25100f28ec92d20f5b7bbe8b89423e2
Marco Secchi <loziosecchi@gmail.com>
2023-04-27 12:19:29 +0200

    Cleanup PlayerState class.

--- Source/UnrealShadows_LOTL/US_PlayerState.cpp
+++ Source/UnrealShadows_LOTL/US_PlayerState.cpp
@@ -7,19 +7,17 @@
 void AUS_PlayerState::AddXp(const int32 Value)
 {
 	Xp += Value;
-	OnXpChanged.Broadcast(Xp);
 
-	// GEngine->AddOnScreenDebugMessage(0, 5.f, FColor::Yellow, FString::Printf(TEXT("Xp: %d"), Value));
+	// GEngine->AddOnScreenDebugMessage(0, 5.f, FColor::Yellow, FString::Printf(TEXT("Total Xp: %d"), Value));
 
 	if (const auto Character = Cast<AUS_Character>(GetPawn()))
 	{
 		if(Character->GetCharacterStats()->NextLevelXp < Xp)
 		{
-			// GEngine->AddOnScreenDebugMessage(3, 5.f, FColor::Red, TEXT("Level Up"));
+			// GEngine->AddOnScreenDebugMessage(3, 5.f, FColor::Red, TEXT("Level Up!"));
 			
 			CharacterLevel++;
 			Character->UpdateCharacterStats(CharacterLevel);
-			OnCharacterLevelUp.Broadcast(CharacterLevel);
 		}
 	}
 }
--- Source/UnrealShadows_LOTL/US_PlayerState.h
+++ Source/UnrealShadows_LOTL/US_PlayerState.h
@@ -24,9 +24,11 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, ReplicatedUsing="OnRep_CharacterLevelUp", Category = "Experience")
 	int CharacterLevel = 1;
 
+	// Called whenever an Xp replication occurs
 	UFUNCTION()
 	void OnRep_Xp(int32 OldValue) const;
 
+	// Called whenever a CharacterLevel replication occurs
 	UFUNCTION()
 	void OnRep_CharacterLevelUp(int32 OldValue) const;
 
@@ -35,13 +37,14 @@ public:
 	/** Adds experience points to the player. */
 	UFUNCTION(BlueprintCallable, Category="Experience")
 	void AddXp(int32 Value);
-	
+
+	// Used to broadcast the experience points changed event
 	UPROPERTY(BlueprintAssignable, Category = "Events")
 	FOnXpChanged OnXpChanged;
 
+	// Used to broadcast the character level up event
 	UPROPERTY(BlueprintAssignable, Category = "Events")
 	FOnCharacterLevelUp OnCharacterLevelUp;
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
-
 };
